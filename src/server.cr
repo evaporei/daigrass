82c27bec9f2b7c7f540a1c2b3cb978985c9e9389
Eva Pace <eba.pachi@gmail.com>
2024-02-19 22:15:23 -0300

    move file access into storage class

--- src/server.cr
+++ src/server.cr
@@ -1,26 +1,22 @@
 require "http/server"
-STORAGE_FILE = ".data"
+require "./storage"
 
-# https://crystal-lang.org/api/1.11.2/File.html#constructor-detail
-storage = File.open STORAGE_FILE, mode: "a+"
+storage = Storage.new
 
 server = HTTP::Server.new do |ctx|
     if ctx.request.method == "POST"
         body = ctx.request.body
         if body
             body_s = body.gets_to_end
-            # File.write STORAGE_FILE, body_s + "\n", mode: "a"
-            storage.print body_s + "\n" # appends because of mode
-            storage.flush # maybe move to SIGINT trap
+            storage.write body_s
             ctx.response.print body_s
             ctx.response.print '\n'
             next
         end
     end
     if ctx.request.method == "GET"
-        # ctx.response.print storage.gets_to_end # consumes buffer
-        ctx.response.print File.read STORAGE_FILE
-        next
+      ctx.response.print storage.read
+      next
     end
     # ctx.response.content_type = "text/json"
     ctx.response.content_type = "text/plain"
