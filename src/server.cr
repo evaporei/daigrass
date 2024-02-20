1d7133dbebcf92a3d23679070116c697bd9c42d0
Eva Pace <eba.pachi@gmail.com>
2024-02-19 22:27:26 -0300

    fmt

--- src/server.cr
+++ src/server.cr
@@ -4,31 +4,31 @@ require "./storage"
 storage = Storage.new
 
 server = HTTP::Server.new do |ctx|
-    if ctx.request.method == "POST"
-        body = ctx.request.body
-        if body
-            body_s = body.gets_to_end
-            storage.write body_s
-            ctx.response.print body_s
-            ctx.response.print '\n'
-            next
-        end
-    end
-    if ctx.request.method == "GET"
-      ctx.response.print storage.read
+  if ctx.request.method == "POST"
+    body = ctx.request.body
+    if body
+      body_s = body.gets_to_end
+      storage.write body_s
+      ctx.response.print body_s
+      ctx.response.print '\n'
       next
     end
-    # ctx.response.content_type = "text/json"
-    ctx.response.content_type = "text/plain"
-    ctx.response.print "default response #{Time.local}"
+  end
+  if ctx.request.method == "GET"
+    ctx.response.print storage.read
+    next
+  end
+  # ctx.response.content_type = "text/json"
+  ctx.response.content_type = "text/plain"
+  ctx.response.print "default response #{Time.local}"
 end
 
 address = server.bind_tcp 5000
 puts "listening on #{address}"
 
 Signal::INT.trap do
-    storage.close
-    server.close
+  storage.close
+  server.close
 end
 
 server.listen
