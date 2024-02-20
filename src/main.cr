34bbceeaf8d39fbef4e52b42d7ad457a5c336825
Eva Pace <eba.pachi@gmail.com>
2024-02-19 22:27:57 -0300

    extract server into class (create main)

---
+++ src/main.cr
@@ -0,0 +1,5 @@
+require "./server"
+
+server = Server.new
+
+server.listen
--- src/server.cr
+++ src/server.cr
@@ -1,34 +1,47 @@
 require "http/server"
 require "./storage"
 
-storage = Storage.new
 
-server = HTTP::Server.new do |ctx|
-  if ctx.request.method == "POST"
-    body = ctx.request.body
-    if body
-      body_s = body.gets_to_end
-      storage.write body_s
-      ctx.response.print body_s
-      ctx.response.print '\n'
-      next
+class Server
+  @address : Socket::IPAddress | Nil
+
+  def initialize()
+    @storage = Storage.new
+
+    @server = HTTP::Server.new do |ctx|
+      self.handler(ctx)
+    end
+
+    @address = @server.not_nil!.bind_tcp 5000
+
+    Signal::INT.trap do
+      @storage.close
+      @server.not_nil!.close
     end
   end
-  if ctx.request.method == "GET"
-    ctx.response.print storage.read
-    next
+
+  def handler(ctx)
+    if ctx.request.method == "POST"
+      body = ctx.request.body
+      if body
+        body_s = body.gets_to_end
+        @storage.write body_s
+        ctx.response.print body_s
+        ctx.response.print '\n'
+        return
+      end
+    end
+    if ctx.request.method == "GET"
+      ctx.response.print @storage.read
+      return
+    end
+    # ctx.response.content_type = "text/json"
+    ctx.response.content_type = "text/plain"
+    ctx.response.print "default response #{Time.local}"
   end
-  # ctx.response.content_type = "text/json"
-  ctx.response.content_type = "text/plain"
-  ctx.response.print "default response #{Time.local}"
+
+  def listen()
+    puts "listening on #{@address}"
+    @server.not_nil!.listen
+  end
 end
-
-address = server.bind_tcp 5000
-puts "listening on #{address}"
-
-Signal::INT.trap do
-  storage.close
-  server.close
-end
-
-server.listen
