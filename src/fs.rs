ca86bdc254cc62af307d01e66872039907ef6c3c
Eva Pace <eba.pachi@gmail.com>
2024-03-05 12:30:42 -0300

    fs: create new blocks when the prev is full

--- src/fs.rs
+++ src/fs.rs
@@ -20,7 +20,7 @@ where
 }
 
 trait Heap {
-    fn create(table: &str) -> Result<Self, io::Error>
+    fn create(table: &str, offset: u64) -> Result<Self, io::Error>
     where
         Self: Sized;
     fn open(table: &str, offset: u64) -> Result<Self, io::Error>
@@ -36,12 +36,38 @@ pub struct HeapFile {
     table: String,
 }
 
+pub struct HeapFileIterator {
+    block: usize,
+    heap: Vec<HeapBlockIterator>,
+}
+
 impl IntoIterator for HeapFile {
     type Item = Result<Row, io::Error>;
-    type IntoIter = HeapIterator;
+    type IntoIter = HeapFileIterator;
 
-    fn into_iter(mut self) -> Self::IntoIter {
-        self.heap.remove(0).into_iter()
+    fn into_iter(self) -> Self::IntoIter {
+        let mut iters = vec![];
+        for heap in self.heap {
+            iters.push(heap.into_iter());
+        }
+        Self::IntoIter {
+            heap: iters,
+            block: 0,
+        }
+    }
+}
+
+impl Iterator for HeapFileIterator {
+    type Item = Result<Row, io::Error>;
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.heap[self.block].next() {
+            Some(row) => Some(row),
+            None => {
+                let next_heap = self.heap.get_mut(self.block + 1)?;
+                self.block += 1;
+                next_heap.next()
+            },
+        }
     }
 }
 
@@ -59,9 +85,21 @@ impl HeapFile {
 
 impl Heap for HeapFile {
     fn insert(&mut self, row: &Row) -> Result<(), io::Error> {
-        self.heap[self.n].insert(row)
+        match self.heap[self.n].insert(row) {
+            Ok(u) => Ok(u),
+            Err(err) if err.kind() == io::ErrorKind::OutOfMemory => {
+                let next = self.n + 1;
+                let offset = 8192 * next as u64;
+                self.heap.push(HeapBlock::create(&self.table, offset)?);
+                self.n = next;
+                self.heap[self.n].insert(row)
+            },
+            Err(err) => Err(err),
+        }
     }
     fn get(&mut self, n: usize) -> Result<Option<Row>, io::Error> {
+        // FIXME: we should consider a different n
+        // for the other blocks
         self.heap[self.n].get(n)
     }
     fn open(table: &str, offset: u64) -> Result<Self, io::Error>
@@ -74,12 +112,12 @@ impl Heap for HeapFile {
             n: 0,
         })
     }
-    fn create(table: &str) -> Result<Self, io::Error>
+    fn create(table: &str, offset: u64) -> Result<Self, io::Error>
     where
         Self: Sized,
     {
         Ok(Self {
-            heap: vec![Heap::create(table)?],
+            heap: vec![Heap::create(table, offset)?],
             table: table.to_owned(),
             n: 0,
         })
@@ -100,13 +138,15 @@ struct HeapBlock {
 }
 
 impl Heap for HeapBlock {
-    fn create(table: &str) -> Result<Self, io::Error> {
+    fn create(table: &str, offset: u64) -> Result<Self, io::Error> {
         let mut file = OpenOptions::new()
             .read(true)
             .write(true)
             .create(true)
             // maybe change folder if cfg(test)?
             .open(format!("./data/{table}"))?;
+
+        file.seek(SeekFrom::Start(offset))?;
 
         // 2 (16 bits) * 2
         let ptr_lower: u16 = 4;
@@ -131,6 +171,7 @@ impl Heap for HeapBlock {
         })
     }
 
+    /// TODO: deprecate
     fn open(table: &str, offset: u64) -> Result<Self, io::Error> {
         let mut file = OpenOptions::new()
             .read(true)
@@ -251,21 +292,21 @@ impl HeapBlock {
     }
 }
 
-pub struct HeapIterator {
+pub struct HeapBlockIterator {
     n: usize,
     heap: HeapBlock,
 }
 
 impl IntoIterator for HeapBlock {
     type Item = Result<Row, io::Error>;
-    type IntoIter = HeapIterator;
+    type IntoIter = HeapBlockIterator;
 
     fn into_iter(self) -> Self::IntoIter {
-        HeapIterator { n: 0, heap: self }
+        Self::IntoIter { n: 0, heap: self }
     }
 }
 
-impl Iterator for HeapIterator {
+impl Iterator for HeapBlockIterator {
     type Item = Result<Row, io::Error>;
     fn next(&mut self) -> Option<Self::Item> {
         let row = self.heap.get(self.n).transpose()?;
@@ -278,7 +319,7 @@ impl Iterator for HeapIterator {
 
 #[test]
 fn test_heap_file() {
-    let heap = HeapFile::create("test_movies").unwrap();
+    let heap = HeapFile::create("test_movies", 0).unwrap();
 
     let expected: [u8; 4] = [0, 4, 32, 0];
     let mut header = [0; 4];
@@ -340,7 +381,7 @@ fn test_heap_file() {
 
 #[test]
 fn test_heap_file_iterator() {
-    let mut heap = HeapFile::create("test_it").unwrap();
+    let mut heap = HeapFile::create("test_it", 0).unwrap();
 
     let movies = vec![
         vec![
@@ -380,6 +421,9 @@ fn test_heap_file_iterator() {
     );
 }
 
+// FIXME: this test no longer is needed
+// now we need to check that iteration/retrieval is working properly
+// with multiple pages
 #[test]
 #[should_panic(expected = "no more space in heap file")]
 fn test_heap_full() {
@@ -393,9 +437,73 @@ fn test_heap_full() {
     // fit the same movie again
     let movies = std::iter::repeat(movie).take(121);
 
-    let mut heap = HeapFile::create("test_full").unwrap();
+    let mut heap = HeapFile::create("test_full", 0).unwrap();
 
     for movie in movies {
         heap.insert(&movie).unwrap();
     }
+}
+
+// same as first test, but with 8192 offset (second block)
+#[test]
+fn test_heap_with_offset() {
+    let heap = HeapFile::create("test_blocks", 8192).unwrap();
+
+    let expected: [u8; 4] = [0, 4, 32, 0];
+    let mut header = [0; 4];
+    let mut f = File::open("./data/test_blocks").unwrap();
+    f.seek(SeekFrom::Start(8192)).unwrap();
+    f.read_exact(&mut header).unwrap();
+    assert_eq!(header, expected);
+    assert_eq!(heap.free_space(), 8188);
+
+    let mut heap = HeapFile::open("test_blocks", 8192).unwrap();
+
+    assert_eq!(heap.ptr_lower(), 4);
+    assert_eq!(heap.ptr_upper(), 8192);
+    // remains the same with ::open()
+    assert_eq!(heap.free_space(), 8188);
+
+    let movie = vec![
+        // - length -  1
+        // 00 00 00 01 31
+        "1".into(),
+        // - length -  T  o  y  \s S  t  o  r  y
+        // 00 00 00 09 54 6f 79 20 53 74 6f 72 79
+        "Toy Story".into(),
+        // - length -  A  n  i  m  a  t  i  o  n
+        // 00 00 00 09 41 6e 69 6d 61 74 69 6f 6e
+        "Animation".into(),
+    ];
+    heap.insert(&movie).unwrap();
+
+    let expected = [
+        0x00, 0x19, // upper length
+        0x00, 0x01, // length
+        0x31, // "1"
+        0x00, 0x09, // length
+        0x54, 0x6f, 0x79, 0x20, 0x53, 0x74, 0x6f, 0x72, 0x79, // "Toy Story"
+        0x00, 0x09, // length
+        0x41, 0x6e, 0x69, 0x6d, 0x61, 0x74, 0x69, 0x6f, 0x6e, // "Animation"
+    ];
+
+    let new_upper = 8192 - expected.len() as u16;
+    assert_eq!(heap.ptr_upper(), new_upper);
+    assert_eq!(heap.ptr_lower(), 6);
+    assert_eq!(heap.free_space(), 8192 - 4 - expected.len() as u16);
+
+    f.seek(SeekFrom::Start((8192 + 8192 - expected.len()) as u64))
+        .unwrap();
+    // f.seek(SeekFrom::End(-(expected.len() as i64))).unwrap();
+    let mut found = [0; 27];
+    f.read_exact(&mut found).unwrap();
+    assert_eq!(found, expected);
+
+    let expected: [u8; 4] = [0, 6, 0x1f, 0xe5];
+    let mut header = [0; 4];
+    f.seek(SeekFrom::Start(8192)).unwrap();
+    f.read_exact(&mut header).unwrap();
+    assert_eq!(header, expected);
+
+    assert_eq!(heap.get(0).unwrap(), Some(movie));
 }
